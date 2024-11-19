ac414a00221556de7e5d8924fdf50bc6c1572665
percar-kurs <per.carlsson@elevmail.stockholm.se>
2024-11-20 00:57:21 +0100

    format

---
+++ tests/store_consumer.spec.ts
@@ -0,0 +1,27 @@
+import { expect, test } from "@playwright/test";
+import { LoginPage } from "../pages/loginpage";
+import { StorePage } from "../pages/storepage";
+
+test('When Login with valid password and logout, Then user is back on login page', async ({ page }) =>
+{
+	const loginPage = new LoginPage(page);
+	const storePage = new StorePage(page);
+
+	let validPassword;
+	if(process.env.PASSWORD !== undefined)
+	{
+		validPassword = process.env.PASSWORD;
+	}
+
+	await page.goto("http://hoff.is/login");
+	await loginPage.login("Markus", validPassword, "consumer");
+	await page.waitForTimeout(1000); // wait for page to be loaded
+
+	const header = await storePage.header;
+	expect(header).toBeVisible();
+
+	// back on login page
+	expect(loginPage.usernameInput).toBeVisible();
+
+
+});
